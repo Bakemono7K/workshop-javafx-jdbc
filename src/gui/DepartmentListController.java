6e75fad0f94e05b72c475d1189ab99ea92e6bd27
Gael <Gabriel.Ordis.Vieira@gmail.com>
2022-05-03 21:38:12 -0300

    Initializing action as parameter

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -60,7 +60,7 @@ public class DepartmentListController implements Initializable {
 
 	}
 	
-	public void UpdateTableView() {
+	public void updateTableView() {
 		if (service == null) {
 			throw new IllegalStateException("Service was null");
 		}
