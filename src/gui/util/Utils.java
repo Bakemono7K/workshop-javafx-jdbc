f442cd475802c56596dafc18e300534ab40b4203
Gael <Gabriel.Ordis.Vieira@gmail.com>
2022-05-06 18:52:45 -0300

    Saving Seller

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -27,6 +27,14 @@ public class Utils {
 			return null;
 		}
 	}
+	
+	public static Double tryParseToDouble(String str) {
+		try {
+			return Double.parseDouble(str);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
 
 	public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) {
 		tableColumn.setCellFactory(column -> {
@@ -89,7 +97,7 @@ public class Utils {
 				} else {
 					return null;
 				}
-			}
+			} 
 		});
 	}
 }
